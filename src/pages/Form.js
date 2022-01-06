221b2a25e0d53d264396aa2d42a3eafc0e5c316a
rmo22 <morecroftr@gmail.com>
2022-01-06 22:08:47 +0000

    Quiz changes

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Form = () => (
 	<div className="wrapper">
-		<h1>How often do you eat pies?</h1>
+		<h1>Quiz... Find out what type of pie you are</h1>
+		<h3>Question 1: Do you like sausage rolls?</h3>
 		<form>
 			<fieldset>
 				<label>
 					<p>Answer</p>
-					<input name="name" />
+					<input name="answer" />
 				</label>
 			</fieldset>
 			<button type="submit">Submit</button>
